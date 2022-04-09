14d979156b49a9a686a98b77e6795a31bbbcdcc0
Thomas Höhenleitner <th@seerose.net>
2022-04-09 17:13:53 +0200

    Update tcobs.go

--- pkg/tcobs/tcobs.go
+++ pkg/tcobs/tcobs.go
@@ -1,6 +1,6 @@
-// Package src is a helper for testing the target C-code.
-// Each C function gets a Go wrapper which ist tested in an appropriate test function.
-// For some reason inside the src_test.go an 'import "C"' is not possible.
+// Package tcobs is also a helper for testing the target C-code.
+// Each C function gets a Go wrapper which ist tested in appropriate test functions.
+// For some reason inside the tcobs_test.go an 'import "C"' is not possible.
 package tcobs
 
 // #include <stdint.h>
