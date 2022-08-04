bffab236aee25e0b95ec3f3009aca18f84be4ae3
rokath <th@seerose.net>
2022-08-04 11:34:22 +0200

    file splitted/renamed

--- TCOBSv1/tcobsDecode.go
+++ TCOBSv1/tcobsDecode.go
@@ -1,29 +1,8 @@
 package tcobs
 
-// #include <stdint.h>
-// #include "tcobs.h"
-// #cgo CFLAGS: -g -Wall
-import "C"
 import (
 	"errors"
-	"unsafe"
 )
-
-// todo: -flto switch does not work on some Windows (Antivirus?) setups.
-
-// CEncode encodes `i` into `o` and returns number of bytes in `o`.
-// For details see https://github.com/rokath/TCOBS/blob/master/docs/TCOBSSpecification.md.
-// The CEncode implementation is done in C because the aimed use case is an embedded device running C.
-// This function is mainly for testing.
-func CEncode(o, i []byte) (n int) {
-	if len(i) == 0 {
-		return
-	}
-	in := unsafe.Pointer(&i[0])
-	out := unsafe.Pointer(&o[0])
-	n = int(C.TCOBSEncode(out, in, C.size_t(len(i))))
-	return
-}
 
 const (
 	N        = 0xA0 // noop sigil byte
@@ -66,7 +45,7 @@ func Decode(d, in []byte) (n int, e error) {
 		next := in[len(in)-1] // get next sigil byte (starting from the end)
 		sigil, offset := sigilAndOffset(next)
 		if offset+1 > len(in) {
-                        n = 0
+			n = 0
 			e = errors.New("sigil chain mismatch")
 			return
 		}
